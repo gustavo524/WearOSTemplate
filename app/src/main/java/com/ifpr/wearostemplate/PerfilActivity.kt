923e5c9c679f623a16ab8ff2edbf2f295cd620a8
pontagrossa-ai <gustavo.hanisch@gmail.com>
2026-06-23 20:30:38 -0300

    tela de perfil

---
+++ app/src/main/java/com/ifpr/wearostemplate/PerfilActivity.kt
@@ -0,0 +1,11 @@
+package com.ifpr.wearostemplate
+
+import android.app.Activity
+import android.os.Bundle
+
+class PerfilActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
+    }
+}

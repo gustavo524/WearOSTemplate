d085d31d2991201e8d74be736f92c997eaa9677d
pontagrossa-ai <gustavo.hanisch@gmail.com>
2026-06-30 20:22:39 -0300

    botao perfil  e botao voltar feitos

--- app/src/main/java/com/ifpr/wearostemplate/PerfilActivity.kt
+++ app/src/main/java/com/ifpr/wearostemplate/PerfilActivity.kt
@@ -2,10 +2,19 @@ package com.ifpr.wearostemplate
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class PerfilActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
+
+        val btnVoltar = findViewById<Button>(R.id.btnVoltar)
+
+        btnVoltar.setOnClickListener {
+            finish()
+        }
     }
 }
